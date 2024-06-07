73d2fa9a41dec0bec1580b90fd2ed1852d994238
Aniket228 <99678324+Aniket228@users.noreply.github.com>
2024-06-07 08:02:40 +0530

    Add files via upload

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#52B947',
+    },
+    secondary: {
+      main: '#233E76',
+    },  
+  },
+});
+
+export default theme;
